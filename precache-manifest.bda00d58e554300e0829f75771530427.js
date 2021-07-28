fbb418410587edd30691b2838e205931f5fd5ffa
Andres <aneduardotejada@gmail.com>
2021-07-27 21:12:18 -0500

    Updates

---
+++ precache-manifest.bda00d58e554300e0829f75771530427.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a7eb32758a1cfdfd40c5f6a55e5643d",
+    "url": "/Journal-App/index.html"
+  },
+  {
+    "revision": "1a5389e6d539251c51ac",
+    "url": "/Journal-App/static/css/main.643bae5c.chunk.css"
+  },
+  {
+    "revision": "b4f9ad105df351a6e5ea",
+    "url": "/Journal-App/static/js/2.c4015b18.chunk.js"
+  },
+  {
+    "revision": "b01cf20ee08477db845d02c3b279da13",
+    "url": "/Journal-App/static/js/2.c4015b18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a5389e6d539251c51ac",
+    "url": "/Journal-App/static/js/main.89fb3336.chunk.js"
+  },
+  {
+    "revision": "6c6aa7f30f8f30ffb131",
+    "url": "/Journal-App/static/js/runtime-main.2a0e8efc.js"
+  }
+]);
